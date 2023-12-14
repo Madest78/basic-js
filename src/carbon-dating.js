963f8744272a754472ded43dc67d93cf7729bde8
Madest78 <Vfnectdbx78@gmail.com>
2023-12-14 17:58:25 +0400

    init: submit task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity !== "string" ||//проверяем является ли аргумент числом в нужном диапазоне
+    isNaN(parseFloat(sampleActivity)) || 
+    parseFloat(sampleActivity) <= 0 ||
+    parseFloat(sampleActivity) > MODERN_ACTIVITY
+  ) {
+    return false;
+  }
+//используем формулу вычисления полураспада радиоактивных веществ найденную в гугле естественно)
+  const k = 0.693 / HALF_LIFE_PERIOD;   // округляем примерный log2 (~0693147)
+  const t = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / k;
+  return Math.ceil(t)
 }
 
 module.exports = {
--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const string1 = s1.split('');
+  const string2 = s2.split('');//порезали строкиб получили массивы символов
+
+  let count = 0;//заводим счетчик
+//считаем общие символы в строках
+ for (let i = 0; i < string1.length; i++) {
+  const index = string2.indexOf(string1[i]);
+  if (index !== -1) {
+    count ++;
+    string2.splice(index,1);//удаляем найденные совпадения из второго массива.
+  }
+ }
+ return count;
 }
 
 module.exports = {
